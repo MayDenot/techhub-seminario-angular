a0f4bb54ea81a0408a5ca8451e37e500f69fd9ac
Mayra Denot <mayra.denot@gmail.com>
2025-07-30 22:15:50 -0300

    Eliminacion de import sin usar

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { provideServerRendering } from '@angular/ssr';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
